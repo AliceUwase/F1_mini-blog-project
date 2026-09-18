6bd9d5a4e0ef5225e740cd668a16a19087e6eb15
AliceUwase <a.uwase3@alustudent.com>
2026-09-18 14:45:31 +0200

    feat: add Header component with navigation

---
+++ mini-blog/src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Dev Insights</h1>
+            <nav>
+                <a href="#new-post">New Post</a>
+            </nav>
+        </header>
+    )
+}
